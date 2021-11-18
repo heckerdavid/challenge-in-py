48129d9449f907ecf3bf88bd75c7bff99c5fdfd7
David Hecker <heckerdw@gmail.com>
2021-11-18 14:10:16 -0800

    Completed C3Q1.

---
+++ challenge-03.py
@@ -0,0 +1,170 @@
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 1 - Review
+
+# Write a function called addTwo that takes in an array and adds two to every value using a for loop. Place the new value in a new array. Return the new array.
+# ------------------------------------------------------------------------------------------------ */
+
+def add_two(array):
+    new_array = []
+
+    for num in array:
+        new_array.append(num + 2)
+
+    return new_array
+
+print(add_two([1, 2, 3, 4, 5]))
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 2
+
+# Write a function named typeNum that, given an array as input, uses filter to return an array containing only the numbers.
+
+# For example, typeNum([1, 'bob' ,3]) returns [1,3].
+# ------------------------------------------------------------------------------------------------ */
+
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 3
+
+# Write a function named containsAnd that, given an array of strings as input, uses filter to return an array containing only strings that contain 'and' within the string.
+
+# For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
+# ------------------------------------------------------------------------------------------------ */
+
+
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 4
+
+# Write a function named oddValues that, given an array of integers as input, uses filter to return an array containing only the odd integers.
+
+# For example, oddValues([1,2,3]) returns [1,3].
+# ------------------------------------------------------------------------------------------------ */
+
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 5
+
+# Write a function named notInFirstArray that, given two arrays as input, uses filter to return an array of all the elements in the second array that are not included in the first array.
+
+# For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
+# ------------------------------------------------------------------------------------------------ */
+
+
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 6 - Stretch Goal
+
+# Write a function named getBaseStatGreaterThan that, given the snorlaxData, below, and an integer as input, uses filter to return an array containing all stats with a baseStat greater than the integer.
+
+# For example, getBaseStatGreaterThan(snorlaxData.stats, 50) will return an array containing the 'special-defense' and 'special-attack' objects.
+# ------------------------------------------------------------------------------------------------ */
+
+# const snorlaxData = {
+#   stats: [
+#     {
+#       stat: {
+#         url: 'https://pokeapi.co/api/v2/stat/6/',
+#         name: 'speed',
+#       },
+#       effort: 5,
+#       baseStat: 30,
+#     },
+#     {
+#       stat: {
+#         url: 'https://pokeapi.co/api/v2/stat/5/',
+#         name: 'special-defense',
+#       },
+#       effort: 2,
+#       baseStat: 110,
+#     },
+#     {
+#       stat: {
+#         url: 'https://pokeapi.co/api/v2/stat/4/',
+#         name: 'special-attack',
+#       },
+#       effort: 9,
+#       baseStat: 65,
+#     },
+#   ],
+#   name: 'snorlax',
+#   weight: 4600,
+# };
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 7 - Stretch Goal
+
+# Write a function named getStatName that is an extension of your getBaseStatGreaterThan function from challenge 7. For this function, extend your solution from challenge 7 to only return the name of the stat, rather than the entire stat object.
+
+# For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 'special-attack'].
+# ------------------------------------------------------------------------------------------------ */
+
+
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 8 - Stretch Goal
+
+# Write a function named getCharactersWithoutChildren that, given the array of characters, below, uses filter to return an array of all characters without children.
+# ------------------------------------------------------------------------------------------------ */
+
+# const characters = [
+#   {
+#     name: 'Eddard',
+#     spouse: 'Catelyn',
+#     children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+#     house: 'Stark',
+#   },
+#   {
+#     name: 'Jon',
+#     spouse: 'Lysa',
+#     children: ['Robin'],
+#     house: 'Arryn',
+#   },
+#   {
+#     name: 'Cersei',
+#     spouse: 'Robert',
+#     children: ['Joffrey', 'Myrcella', 'Tommen'],
+#     house: 'Lannister',
+#   },
+#   {
+#     name: 'Daenarys',
+#     spouse: 'Khal Drogo',
+#     children: ['Drogon', 'Rhaegal', 'Viserion'],
+#     house: 'Targaryen',
+#   },
+#   {
+#     name: 'Mace',
+#     spouse: 'Alerie',
+#     children: ['Margaery', 'Loras'],
+#     house: 'Tyrell',
+#   },
+#   {
+#     name: 'Sansa',
+#     spouse: 'Tyrion',
+#     house: 'Stark',
+#   },
+#   {
+#     name: 'Jon',
+#     spouse: null,
+#     house: 'Snow',
+#   },
+# ];
+
+
+
+# /* ------------------------------------------------------------------------------------------------
+# CHALLENGE 9 - Stretch Goal
+
+# Write a function named evenOddNumericValues that, given an array as input, uses filter to remove any non-numeric values, then uses map to generate a new array containing the string 'even' or 'odd', depending on the original value.
+
+# For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 'odd'].
+# ------------------------------------------------------------------------------------------------ */
+
